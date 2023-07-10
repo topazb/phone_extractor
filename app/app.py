9593ebda2e1fce9d2ed13497ab172a5aa1508ede
Topaz Braverman <topaz.braverman@anaplan.com>
2023-07-10 14:10:36 +0300

    added exclude numbers

--- app/app.py
+++ app/app.py
@@ -18,15 +18,20 @@ def process_text():
         # Handle the POST request and extract phone numbers
         text1 = request.json['text1']
         text2 = request.json['text2']
-        text3 = request.json['text3']
         num_lists = request.json['num_lists']
+        exclude_numbers = request.json['exclude_numbers']
 
         phone_numbers1 = extract_phone_numbers(text1)
         phone_numbers2 = extract_phone_numbers(text2)
-        phone_numbers3 = extract_phone_numbers(text3)
 
-        # Subtract the phone numbers in text 3 from text 2
-        active_instructors = subtract_phone_numbers(phone_numbers2, phone_numbers3)
+        # Format the exclude_numbers
+        formatted_exclude_numbers = format_phone_numbers(exclude_numbers)
+
+        # Subtract the formatted_exclude_numbers from phone_numbers1
+        phone_numbers1 = subtract_phone_numbers(phone_numbers1, formatted_exclude_numbers)
+
+        # Subtract the phone numbers in text 2 from formatted_exclude_numbers
+        active_instructors = subtract_phone_numbers(phone_numbers2, formatted_exclude_numbers)
 
         if num_lists > 0:
             divided_lists = divide_phone_numbers(phone_numbers1, num_lists)  # Divide into num_lists lists
@@ -37,8 +42,7 @@ def process_text():
 
         # Update num_lists if it is 0 or active_instructors is empty
         else:
-            num_lists = max(1,
-                            num_lists)  # Set num_lists to the length of active_instructors or at least 1
+            num_lists = max(1, num_lists)  # Set num_lists to the length of active_instructors or at least 1
             divided_lists = divide_phone_numbers(phone_numbers1, num_lists)  # Divide into num_lists lists
 
         # Prepare the response
@@ -46,11 +50,9 @@ def process_text():
             'num_phones': len(phone_numbers1),
             'phone_lists': divided_lists,
             'list2_length': len(phone_numbers2),
-            'active_instructors_length': len(active_instructors),  # Length of active_instructors
-            'active_instructors': active_instructors  # active_instructors
-
+            'active_instructors_length': len(active_instructors),
+            'active_instructors': active_instructors
         }
-
         # Return the response
         return jsonify(response_data)
 
@@ -98,5 +100,19 @@ def divide_phone_numbers(phone_numbers, num_lists):
 
     return divided_lists
 
+def format_phone_numbers(phone_numbers):
+    formatted_numbers = []
+    for number in phone_numbers:
+        # Remove the leading '0' and add '+972' as prefix
+        formatted_number = '+972 ' + number[1:]
+
+        # Add '-' after 2 digits and another '-' after 3 more digits
+        formatted_number = formatted_number[:7] + '-' + formatted_number[7:10] + '-' + formatted_number[10:]
+
+        formatted_numbers.append(formatted_number)
+        print(formatted_number)
+
+    return formatted_numbers
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
