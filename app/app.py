3efbfc8b415d8e3b46a6c72c435a4b1c2144ee0b
Topaz Braverman <topaz.braverman@anaplan.com>
2023-07-17 15:43:07 +0300

    fixed text2 to be names

--- app/app.py
+++ app/app.py
@@ -23,7 +23,6 @@ def process_text():
         exclude_numbers = request.json['exclude_numbers']
 
         phone_numbers1 = extract_phone_numbers(text1)
-        phone_numbers2 = extract_phone_numbers(text2)
         phone_numbers_instructors = extract_phone_numbers_sheet()
 
         # Format the exclude_numbers
@@ -33,29 +32,18 @@ def process_text():
         # Subtract the formatted_exclude_numbers_instructors from phone_numbers1
         phone_numbers1 = subtract_phone_numbers(phone_numbers1, formatted_exclude_numbers + formatted_exclude_numbers_instructors)
 
-
-        # Subtract the phone numbers in text 2 from formatted_exclude_numbers
-        active_instructors = subtract_phone_numbers(phone_numbers2, formatted_exclude_numbers)
-
         if num_lists > 0:
             divided_lists = divide_phone_numbers(phone_numbers1, num_lists)  # Divide into num_lists lists
-
-        # Update divided_lists if active_instructors is not empty
-        elif len(active_instructors) > 0:
-            divided_lists = divide_phone_numbers(phone_numbers1, len(active_instructors))
-
-        # Update num_lists if it is 0 or active_instructors is empty
         else:
-            num_lists = max(1, num_lists)  # Set num_lists to the length of active_instructors or at least 1
+            num_lists = max(1, num_lists)  # Set num_lists to at least 1
             divided_lists = divide_phone_numbers(phone_numbers1, num_lists)  # Divide into num_lists lists
 
         # Prepare the response
+        text2_items = extract_text2_items(text2)  # Extract items from text2
         response_data = {
-            'num_phones': len(phone_numbers1)+1,
+            'num_phones': len(phone_numbers1),
             'phone_lists': divided_lists,
-            'list2_length': len(phone_numbers2),
-            'active_instructors_length': len(active_instructors),
-            'active_instructors': active_instructors
+            'text2_items': text2_items
         }
         # Return the response
         return jsonify(response_data)
@@ -114,9 +102,14 @@ def format_phone_numbers(phone_numbers):
         formatted_number = formatted_number[:7] + '-' + formatted_number[7:10] + '-' + formatted_number[10:]
 
         formatted_numbers.append(formatted_number)
-       #print(formatted_number)
 
     return formatted_numbers
+
+def extract_text2_items(text):
+    # Split text2 into items based on newline characters
+    items = text.strip().split('\n')
+
+    return items
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
